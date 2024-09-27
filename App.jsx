22cca5d71d91e8e4a02900d93f247d50ec94d089
mamad harp <msh13815@gmail.com>
2024-09-28 00:38:02 +0330

    add 99% module and extensions

--- App.jsx
+++ App.jsx
@@ -1,11 +1,31 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './src/Screens/Home';
+
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+
+    <GestureHandlerRootView style={{ flex: 1 }} >
+
+      <NavigationContainer>
+
+        <Stack.Navigator screenOptions={{headerShown : false ,}} >
+
+          <Stack.Screen name='HOME_SCREEN' component={Home} />
+
+        </Stack.Navigator>
+
+      </NavigationContainer>
+
+    </GestureHandlerRootView>
+    
+
   )
 }
 
---
+++ src/Screens/Home.jsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+//Awsome Slider
+import { useSharedValue } from 'react-native-reanimated';
+import { Slider } from 'react-native-awesome-slider';
+
+
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
